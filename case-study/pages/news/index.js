a543e35dca6440ffa9cce4c053cd55afe9d1d8d7
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-01-28 13:52:34 -0300

    Link for Single Page routing

--- case-study/pages/news/index.js
+++ case-study/pages/news/index.js
@@ -1,7 +1,20 @@
-// our-domain.com/news
+import Link from 'next/link';
+import { Fragment } from "react";
 
 function NewsPage(){
-    return <h1>The News Page</h1>
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/nextjs-is-a-great-framework'>
+                        NextJS is a great Framework
+                    </Link>
+                </li>
+                <li>Something Else</li>
+            </ul>
+        </Fragment>
+    );
 }
 
 export default NewsPage;
