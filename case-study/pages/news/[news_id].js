7cfdaca232d1b770a280fd9da16f827a548f30fe
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-01-28 13:39:32 -0300

    Querying URL params

--- case-study/pages/news/[news_id].js
+++ case-study/pages/news/[news_id].js
@@ -1,6 +1,14 @@
-// our-domain.com/news
+import { useRouter } from 'next/router';
 
 function DetailPage(){
+    const router = useRouter();
+    const news_id = router.query.news_id;
+
+    // send a request to the backend API
+    // to fetch the news item with news_id
+
+    console.log(news_id);
+
     return <h1>The Detail Page</h1>
 }
 
