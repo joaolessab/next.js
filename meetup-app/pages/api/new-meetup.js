91d94a1c2102f0c67e10b6e5def869a2e9f31e40
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-02-01 12:39:26 -0300

    Connecting MongoDB

--- meetup-app/pages/api/new-meetup.js
+++ meetup-app/pages/api/new-meetup.js
@@ -1,4 +1,5 @@
 // api/new/meetup
+import { MongoClient } from 'mongodb';
 
 function handler(req, res){
     if (req.method === 'POST'){ // POST is the only method allowed here
@@ -6,7 +7,8 @@ function handler(req, res){
 
         const { title, image, address, description } = data;
 
-
+        // You will never want to share this into public folders, but here there's no problem because the user will never see it
+        MongoClient.connect('mongodb+srv://joaolessab:nextjsapp@cluster0.1euve.mongodb.net/meetups?retryWrites=true&w=majority');
     }
 }
 
