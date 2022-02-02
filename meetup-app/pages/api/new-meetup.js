9c9d0b4462707bf4c5a93e507159842dc6bb74ca
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-02-02 02:14:26 -0300

    Improvement of API post function

--- meetup-app/pages/api/new-meetup.js
+++ meetup-app/pages/api/new-meetup.js
@@ -5,17 +5,24 @@ async function handler(req, res){
     if (req.method === 'POST'){ // POST is the only method allowed here
         const data = req.body;
 
-        // You will never want to share this into public folders, but here there's no problem because the user will never see it
-        const client = await MongoClient.connect('mongodb+srv://joaolessab:nextjsapp@cluster0.1euve.mongodb.net/meetups?retryWrites=true&w=majority');
-        const db = client.db;
+        try{
+            const uri = 'mongodb+srv://joaolessab:p0dnfmwB1wrAP89o@cluster0.1euve.mongodb.net/meetups?retryWrites=true&w=majority'; 
+            // You will never want to share credentials into public folders, but here there's no problem because the user will never see it
+            const client = await MongoClient.connect(uri);
+            const db = client.db();
 
-        const meetupsCollection = db.collection('meetups');
-        const result = await meetupsCollection.insertOne(data);
+            const meetupsCollection = db.collection('meetups');
+            const result = await meetupsCollection.insertOne(data);
 
-        console.log(result);
-        client.close();
+            console.log(result);
+            client.close();
 
-        res.status(201).json({ message: 'Meetup inserted!' });
+            res.status(201).json({message: 'Meetup inserted!'});
+        }
+        catch(error){
+            console.log('Error to reach API');
+            console.log('Error: ' + error);
+        }
     }
 }
 
