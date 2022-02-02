2a5beca0082ad90809c1bec9080adb4791b4ed65
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2022-02-02 02:44:29 -0300

    Router redirect after calling API

--- meetup-app/pages/new-meetup/index.js
+++ meetup-app/pages/new-meetup/index.js
@@ -1,7 +1,10 @@
 // our-domain.com/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage(){
+    const router = useRouter();
+
     async function addMeetupHandler(enteredMeetupData){
         const response = await fetch('/api/new-meetup', {
             method: 'POST',
@@ -13,6 +16,8 @@ function NewMeetupPage(){
 
         const data = await response.json();
         console.log(data);
+
+        router.push('/');
     }
 
     return <NewMeetupForm onAddMeetup = {addMeetupHandler} />;
